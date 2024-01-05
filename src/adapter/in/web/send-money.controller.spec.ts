1f2467143e3307e10c71979745bac44a7cb9f6f9
dh.kang <dh.kang@woowahan.com>
2024-01-05 21:53:02 +0900

    test: add test for web adapter

---
+++ src/adapter/in/web/send-money.controller.spec.ts
@@ -0,0 +1,35 @@
+import request from 'supertest';
+import { Test, TestingModule } from '@nestjs/testing';
+import { SendMoneyController } from './send-money.controller';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { SendMoneyCommand, SendMoneyUseCase } from 'account/application/port/in';
+import { AccountId, Money } from 'account/domain';
+
+describe('SendMoneyController', () => {
+  let app: INestApplication;
+  let sendMoneyUseCase: SendMoneyUseCase = { sendMoney: jest.fn() };
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      controllers: [SendMoneyController],
+      providers: [{ provide: 'SendMoneyUseCase', useValue: sendMoneyUseCase }],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('should success', async () => {
+    const response = await request(app.getHttpServer()).post('/accounts/send/1/2/500');
+
+    expect(response.status).toBe(HttpStatus.CREATED);
+    expect(sendMoneyUseCase.sendMoney).toHaveBeenCalledTimes(1);
+    expect(sendMoneyUseCase.sendMoney).toHaveBeenCalledWith(
+      new SendMoneyCommand(new AccountId(1), new AccountId(2), Money.of(500)),
+    );
+  });
+});
--- src/adapter/in/web/send-money.controller.ts
+++ src/adapter/in/web/send-money.controller.ts
@@ -1,10 +1,10 @@
-import { Controller, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Controller, Inject, Param, ParseIntPipe, Post } from '@nestjs/common';
 import { SendMoneyCommand, SendMoneyUseCase } from 'account/application/port/in';
 import { AccountId, Money } from 'account/domain';
 
 @Controller()
 export class SendMoneyController {
-  constructor(private readonly sendMoneyUseCase: SendMoneyUseCase) {}
+  constructor(@Inject('SendMoneyUseCase') private readonly sendMoneyUseCase: SendMoneyUseCase) {}
 
   @Post('/accounts/send/:sourceAccountId/:targetAccountId/:amount')
   sendMoney(
